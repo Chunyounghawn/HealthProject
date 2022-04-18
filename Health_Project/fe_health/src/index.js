9d06bce4c443a45a5def0fc9396ade5bfa939c44
Chunyounghawn <zing0982@naver.com>
2022-04-18 15:52:39 +0900

    fe_fullpage 적용중/비디오에러

--- Health_Project/fe_health/src/index.js
+++ Health_Project/fe_health/src/index.js
@@ -2,9 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import ReactFullpage from "@fullpage/react-fullpage";
+
+import "./style.css";
+
+
+class MySection extends React.Component {
+  render() {
+    return (
+      <div className="section">
+        <h3>{this.props.content}</h3>
+          <video id="myVideo" loop muted playsInline data-autoplay >
+            <source src="video/aa.mp4" type="video/mp4"/>
+          </video>
+      </div>
+
+    );
+  }
+}
+
+const anchors = ["firstPage", "secondPage", "thirdPage"];
+
+const FullpageWrapper = () => (
+  <ReactFullpage
+    anchors={anchors}
+    navigation
+    navigationTooltips={anchors}
+    sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+    onLeave={(origin, destination, direction) => {
+      console.log("onLeave event", { origin, destination, direction });
+    }}
+    render={({ state, fullpageApi }) => {
+      console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
+
+      return (
+        <div>
+          <MySection content={"Slide down!"} />
+          <MySection content={"Keep going!"} />
+          <MySection content={"Slide up!"} />
+        </div>
+      );
+    }}
+  />
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FullpageWrapper />
   </React.StrictMode>,
   document.getElementById('root')
 );
