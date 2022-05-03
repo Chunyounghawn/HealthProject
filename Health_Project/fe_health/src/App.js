46564858a34ea71f5f22d3d31419ab1c3dd69533
JangJinWon <wlsdnjs156@naver.com>
2022-05-03 10:37:28 +0900

    .

--- Health_Project/fe_health/src/App.js
+++ Health_Project/fe_health/src/App.js
@@ -3,24 +3,203 @@ import main_fisrt from "./video/main_fisrt.mp4"
 import ReactFullpage from "@fullpage/react-fullpage"
 import styled from "styled-components"
 
-import calender_image from "./image/calender.jpg";
-import Inbody_image from "./image/in-body.jpg";
-import scheduling_image from "./image/scheduling.jpg";
-import water_alarm_image from "./image/water-alarm.jpg";
+import calender_image from "./image/calender.jpg"
+import Inbody_image from "./image/in-body.jpg"
+import scheduling_image from "./image/scheduling.jpg"
+import water_alarm_image from "./image/water-alarm.jpg"
 
 import "./style.css"
 import "./css/section6.css"
 import "./css/section7.css"
-import "./js/section6.js";
+import "./js/section6.js"
 
-import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import { Swiper, SwiperSlide } from "swiper/react" // basic
 
 //import { options, fullpage_api } from "fullpage.js/dist/fullpage.extensions.min"
+
+const Container = styled.div`
+  width: 1740px;
+  height: 100%;
+  position: relative;
+`
+
+const Swiper_phone = styled.div`
+  width: 350px;
+  height: 600px;
+  background-color: yellow;
+  border: 4px solid #333;
+  border-radius: 10%;
+  position: absolute;
+  top: 200px;
+  left: 250px;
+`
+
+const Swiper_slide = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`
+
+const Swiper_prev = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 2px solid #333;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.4s;
+  position: absolute;
+  left: 160px;
+  top: 470px;
+  &: hover {
+    cursor: pointer;
+    background-color: #333;
+    color: #fff;
+  }
+`
+
+const Swiper_next = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 2px solid #333;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.4s;
+  position: absolute;
+  left: 650px;
+  top: 470px;
+  &: hover {
+    cursor: pointer;
+    background-color: #333;
+    color: #fff;
+  }
+`
+
+const ItemBox = styled.div`
+  width: 200px;
+  height: 200px;
+  backface-visibility: hidden;
+  transition: 1.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  border: 2px solid #333;
+  background-color: purple;
+  border-radius: 50%;
+  font-size: 30px;
+`
+
+const Front1 = styled(ItemBox)`
+  position: absolute;
+  top: 200px;
+  left: 1150px;
+  transform: rotateY(0deg);
+`
+
+const Back1 = styled(ItemBox)`
+  position: absolute;
+  top: 200px;
+  left: 1150px;
+  font-size: 15px;
+  background-color: #333;
+  color: #fff;
+  transform: rotateY(-180deg);
+`
+
+const Front2 = styled(ItemBox)`
+  transform: rotateY(0deg);
+`
+
+const Back2 = styled(ItemBox)`
+  font-size: 15px;
+  color: #fff;
+  background-color: #333;
+  transform: rotateY(-180deg);
+`
+
+const Front3 = styled(ItemBox)`
+  transform: rotateY(0deg);
+`
+
+const Back3 = styled(ItemBox)`
+  font-size: 15px;
+  color: #fff;
+  background-color: #333;
+  transform: rotateY(-180deg);
+`
+
+const Front4 = styled(ItemBox)`
+  font-size: 30px;
+  transform: rotateY(0deg);
+`
+
+const Back4 = styled(ItemBox)`
+  font-size: 15px;
+  color: #fff;
+  background-color: #333;
+  transform: rotateY(-180deg);
+`
+
+// Selecter가 hover 되면 Selecter는 180도 회전하면 안되고 Front & Back 이 180도 회전 되야됨
+const Selecter = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 2px solid #333;
+  border-radius: 50%;
+`
+const Selecter1 = styled(Selecter)`
+  &: hover ${Front1} {
+    transform: rotateY(180deg);
+  }
+  &: hover ${Back1} {
+    transform: rotateY(0deg);
+  }
+`
+
+const Selecter2 = styled(Selecter)`
+  position: absolute;
+  top: 400px;
+  left: 950px;
+  &: hover ${Front2} {
+    transform: rotateY(180deg);
+  }
+  &: hover ${Back2} {
+    transform: rotateY(0deg);
+  }
+`
+
+const Selecter3 = styled(Selecter)`
+  position: absolute;
+  top: 400px;
+  left: 1350px;
+  &: hover ${Front3} {
+    transform: rotateY(180deg);
+  }
+  &: hover ${Back3} {
+    transform: rotateY(0deg);
+  }
+`
+
+const Selecter4 = styled(Selecter)`
+  position: absolute;
+  top: 600px;
+  left: 1150px;
+  &: hover ${Front4} {
+    transform: rotateY(180deg);
+  }
+  &: hover ${Back4} {
+    transform: rotateY(0deg);
+  }
+`
 
 function MainSection(props) {
   return (
     <div className="section">
-
       <h3>{props.content}</h3>
       <video id="myVideo" loop muted data-autoplay>
         <source src={main_fisrt} type="video/mp4" />
@@ -72,46 +251,54 @@ function QuestionSection(props) {
 }
 
 function LocationSection(props) {
-  return (    
+  return (
     <div className="section">
+      <Container>
+        <Swiper_phone>
+          <Swiper_slide>
+            <img
+              src={Inbody_image}
+              alt="진단표 스캔"
+              width="300px"
+              height="520px"
+            ></img>
+          </Swiper_slide>
+        </Swiper_phone>
 
-      <div class="container">        
-        <div class="swiper-container">
-          <div class="swiper-phone">
-            <div class="swiper-slide">
-                <img src={Inbody_image} alt="진단표 스캔" width="300px" height="520px"></img>
-            </div>
-          </div>
-        </div>
-
-        <div class="swiper-prev">
+        <Swiper_prev>
           <div class="material-icons">arrow_back_ios</div>
-        </div>
+        </Swiper_prev>
 
-        <div class="swiper-next">
+        <Swiper_next>
           <div class="material-icons">arrow_forward_ios</div>
-        </div>
+        </Swiper_next>
 
-        <div class="item1"> 
-          <div class="front">진단표 스캔</div>
-          <div class="back">카메라로 진단표를 스캔하여 나의 인다비 정보를 확인할 수 있습니다.</div>
-        </div>
+        <Selecter1>
+          <Front1>진단표 스캔</Front1>
+          <Back1>
+            카메라로 진단표를 스캔하여 나의 인다비 정보를 확인할 수 있습니다.
+          </Back1>
+        </Selecter1>
 
-        <div class="item2"> 
-          <div class="front">물 알람</div>
-          <div class="back">알람 기능을 이용하여 사용자가 직접 물 알람을 설정할 수 있습니다.</div>
-        </div>
+        <Selecter2>
+          <Front2>물 알람</Front2>
+          <Back2>
+            알람 기능을 이용하여 사용자가 직접 물 알람을 설정할 수 있습니다.
+          </Back2>
+        </Selecter2>
 
-        <div class="item3"> 
-          <div class="front">운동 스케줄링</div>
-          <div class="back">사용자가 직접 운동 스케줄을 추가 및 삭제할 수 있습니다.</div>
-        </div>
+        <Selecter3>
+          <Front3>운동 스케줄링</Front3>
+          <Back3>사용자가 직접 운동 스케줄을 추가 및 삭제할 수 있습니다.</Back3>
+        </Selecter3>
 
-        <div class="item4"> 
-          <div class="front">캘린더</div>
-          <div class="back">사용자는 캘린더를 통해 해당 날짜의 운동 달성량을 파악할 수 있습니다.</div>
-        </div>
-      </div>
+        <Selecter4>
+          <Front4>캘린더</Front4>
+          <Back4>
+            사용자는 캘린더를 통해 해당 날짜의 운동 달성량을 파악할 수 있습니다.
+          </Back4>
+        </Selecter4>
+      </Container>
 
       <div class="navigation"></div>
     </div>
@@ -121,23 +308,28 @@ function LocationSection(props) {
 function AppStrengthSection(props) {
   return (
     <div className="section">
-
       <div class="container">
         <h3>자주 묻는 질문</h3>
 
         <div class="information1">
           <div class="questions">로그인은 어디서 하나요?</div>
-          <div class="answer">오른쪽 상단 내정보 아이콘에서 로그인 가능 합니다.</div>
+          <div class="answer">
+            오른쪽 상단 내정보 아이콘에서 로그인 가능 합니다.
+          </div>
         </div>
 
         <div class="information2">
           <div class="questions">내 정보는 어디서 확인할 수 있나요?</div>
-          <div class="answer">오른쪽 상단 내정보 아이콘에서 확인 가능 합니다.</div>
+          <div class="answer">
+            오른쪽 상단 내정보 아이콘에서 확인 가능 합니다.
+          </div>
         </div>
 
         <div class="information3">
           <div class="questions">캘린더 수정은 어떻게 하나요?</div>
-          <div class="answer">오른쪽 상단 내정보 아이콘에서 캘린더 탭 클릭하면 수정 가능 합니다.</div>
+          <div class="answer">
+            오른쪽 상단 내정보 아이콘에서 캘린더 탭 클릭하면 수정 가능 합니다.
+          </div>
         </div>
 
         <div class="swiper-prev2">
@@ -149,8 +341,11 @@ function AppStrengthSection(props) {
         </div>
 
         <div class="admin">
-         <div class="inquiry">자주 묻는 질문 외에 다른 질문을 문의 하고 싶다면 아래 버튼을 클릭하여 관리자에게 직접 문의 해주세요.</div>
-         <div class="btn">관리자에게 문의</div>
+          <div class="inquiry">
+            자주 묻는 질문 외에 다른 질문을 문의 하고 싶다면 아래 버튼을
+            클릭하여 관리자에게 직접 문의 해주세요.
+          </div>
+          <div class="btn">관리자에게 문의</div>
         </div>
       </div>
 
@@ -158,8 +353,6 @@ function AppStrengthSection(props) {
     </div>
   )
 }
-
-
 
 function Tnavigation(fullpageApi) {
   return (
@@ -218,12 +411,12 @@ const FullpageWrapper = () => (
 
       return (
         <div>
-          <button onClick={() => 
-          fullpageApi.moveSectionDown()} 
-          id="silentMoveTo"> Click </button>
+          <button onClick={() => fullpageApi.moveTo(6)} id="silentMoveTo">
+            {" "}
+            Click{" "}
+          </button>
 
           <TotalPage fullpageApi={fullpageApi} />
-
         </div>
       )
     }}
