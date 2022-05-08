8bf436a06e468c40a4c04919a2c4fc3e33fb03b7
Chunyounghawn <zing0982@naver.com>
2022-05-09 02:50:08 +0900

    fe_상단버튼수정중

--- Health_Project/fe_health/src/image/index.js
+++ Health_Project/fe_health/src/image/index.js
@@ -1,4 +1,7 @@
 
+const Light = require('../image/nav_icon/light.png')
+const User = require('../image/nav_icon/user.png')
+
 const Main = require('../image/nav_icon/main.png');
 const Thumbs_up = require('../image/nav_icon/thumbs_up.png');
 const Placeholder = require('../image/nav_icon/placeholder.png');
@@ -12,5 +15,10 @@ const Scheduling = require('../image/app/scheduling.jpg')
 const Water_alarm = require('../image/app/water_alarm.jpg')
 
 export {
-  Main, Thumbs_up, Placeholder, Headphone, Reviews, In_body, Calender, Scheduling, Water_alarm
+  //상단 버튼두개
+  Light, User,
+  //중간 네비게이션
+  Main, Thumbs_up, Placeholder, Headphone, Reviews,
+  //앱 장점쪽
+  In_body, Calender, Scheduling, Water_alarm
 }
