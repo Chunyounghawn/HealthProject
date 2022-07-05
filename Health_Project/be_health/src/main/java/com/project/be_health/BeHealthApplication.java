bd27220581c10169fba15291f619e7de95a5b349
swhyeon98 <smreo75@gmail.com>
2022-07-05 18:55:07 +0900

    게시판 코드

--- Health_Project/be_health/src/main/java/com/project/be_health/BeHealthApplication.java
+++ Health_Project/be_health/src/main/java/com/project/be_health/BeHealthApplication.java
@@ -1,22 +1,14 @@
 package com.project.be_health;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
+//@EnableJpaAuditing // JAP Auditing 활성화
 @SpringBootApplication
 public class BeHealthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeHealthApplication.class, args);
 	}
-
 }
